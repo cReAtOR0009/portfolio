3ee9f9cc35e9ae10063950aaf45a05814096c685
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-11 17:15:41 +0100

    text back to left like others

--- src/components/Home/Experience.jsx
+++ src/components/Home/Experience.jsx
@@ -65,7 +65,7 @@ const Experience = () => {
         {/* <p className={`${styles.sectionSubText} text-center`}>
           What I have done so far
         </p> */}
-        <h2 className={`${styles.sectionHeadText} text-center`}>
+        <h2 className={`${styles.sectionHeadText}`}>
           Work Experience.
         </h2>
       </motion.div>

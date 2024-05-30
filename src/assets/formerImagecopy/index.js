4e59f5aefa549d23eb2bd8f67aee713989f64203
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-30 18:26:46 +0100

    changed image assets to optimized lesser quality

---
+++ src/assets/formerImagecopy/index.js
@@ -0,0 +1,170 @@
+import logo from "./logo.jpeg";
+import backend from "./backend.jpeg";
+import creator from "./creator.jpeg";
+import mobile from "./mobile.jpeg";
+import web from "./web.jpeg";
+import react from "./react.jpeg";
+import node from "./node.jpeg"; 
+import contentcreation from "./contentcreation.jpeg";
+import customer from "./customer.jpeg";
+import github from "./github.jpeg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import illustration from "./illustration.jpg";
+import illustration1 from "./Static-website.svg";
+import illustration2 from "./Static-website.jpeg";
+import css from "./tech/css.jpeg";
+import docker from "./tech/docker.jpeg";
+import figma from "./tech/figma.jpeg";
+import git from "./tech/git.jpeg";
+import html from "./tech/html.jpeg";
+import javascript from "./tech/javascript.jpeg";
+import mongodb from "./tech/mongodb.jpeg";
+import nodejs from "./tech/nodejs.jpeg";
+import reactjs from "./tech/reactjs.jpeg";
+import redux from "./tech/redux.jpeg";
+import tailwind from "./tech/tailwind.jpeg";
+import typescript from "./tech/typescript.jpeg";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.jpeg";
+import shopify from "./company/shopify.jpeg";
+import starbucks from "./company/starbucks.jpeg";
+import tesla from "./company/tesla.jpeg";
+
+import whatsapp_icon from "./socials/whatsapp_icon.jpg";
+import twitter from "./socials/twitter_icon.jpg";
+import telegram_icon from "./socials/telegram_icon.jpg";
+import linkdin_icon from "./socials/linkdln_icon.jpg";
+
+import carrent from "./carrent.jpeg";
+import jobit from "./jobit.jpeg";
+import tripguide from "./tripguide.jpeg";
+import portfolio from "./portfoliowebsite.jpeg";
+import blog1 from "./blog1.jpeg";
+import disney from "./disneyclone.jpeg";
+import kasuwa from "./kasuwa.jpeg";
+import deyspeak from "./deyspeak.jpeg";
+import foodorder from "./foodordering.jpeg";
+import groceryapp from "./groceryapp.jpeg";
+import creatornikestore from "./creatornikestore.jpeg";
+import screencapture from "./screencapture.jpeg";
+import realestateImage1 from "./realestateImage1.jpeg";
+import qrcodevoting1 from "./qrCodeVoting1.jpeg";
+import qrcodevoting2 from "./qrCodeVoting2.jpeg";
+import qrcodevoting3 from "./qrCodeVoting3.jpeg";
+import qrcodevoting4 from "./qrCodeVoting4.jpeg";
+import qrcodevoting5 from "./qrCodeVoting5.jpeg";
+
+import web3Course from "./web3Course.jpeg";
+import votingApp from "./votingApp.jpeg";
+import crowdFundingApp from "./crowdFunding.jpeg";
+import TelegramBot from "./TelegramBot.jpeg";
+
+//ipfs project images
+import ipfs1 from "./ipfs1.jpeg";
+import ipfs2 from "./ipfs2.jpeg";
+import ipfs3 from "./ipfs3.jpeg";
+import ipfs4 from "./ipfs4.jpeg";
+import ipfs5 from "./ipfs5.jpeg";
+import ipfs6 from "./ipfs6.jpeg";
+import ipfs7 from "./ipfs7.jpeg";
+
+//nitroBot game Play
+import nitrobot1 from "./nitrobot1.jpeg";
+import nitrobot2 from "./nitrobot2.jpeg";
+import nitrobot3 from "./nitrobot3.jpeg";
+import nitrobot4 from "./nitrobot4.jpeg";
+
+//token Deployment
+import creatortoken from "./creatortoken.jpeg"
+import creatortoken2 from "./creatortoken2.jpeg"
+
+//3d Product website
+import threeDtool from "./3dtool.jpeg"
+import threeDtool2 from "./3dtool2.jpeg"
+
+export {
+  illustration,
+  illustration1,
+  illustration2,
+  react,
+  node,
+  contentcreation,
+  customer,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  whatsapp_icon,
+  telegram_icon,
+  linkdin_icon,
+  twitter,
+  portfolio,
+  disney,
+  kasuwa,
+  deyspeak,
+  foodorder,
+  groceryapp,
+  creatornikestore,
+  blog1,
+  screencapture,
+  realestateImage1,
+  qrcodevoting1,
+  qrcodevoting2,
+  qrcodevoting3,
+  qrcodevoting4,
+  qrcodevoting5,
+  web3Course,
+  votingApp,
+  crowdFundingApp,
+  TelegramBot,
+
+  //ipfs images export
+  ipfs1,
+  ipfs2,
+  ipfs3,
+  ipfs4,
+  ipfs5,
+  ipfs6,
+  ipfs7,
+
+  //nitrobot GamePlay
+  nitrobot1,
+  nitrobot2,
+  nitrobot3,
+  nitrobot4,
+
+  //token deployment
+  creatortoken,
+  creatortoken2,
+
+  //3d  tool Product website
+  threeDtool,
+  threeDtool2
+};

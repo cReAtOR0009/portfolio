11bd5b3335a66fbaf12590187006daf33d16d587
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-09-15 02:11:49 +0100

    added theme to wrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,16 +2,19 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
+import { ThemeContext } from "../context/themeContext";
+import { useContext } from "react";
 
-const StarWrapper = (Component, idName) =>
+const StarWrapper = (Component, idName, bg) =>
   function HOC() {
+    const { theme } = useContext(ThemeContext);
     return (
       <motion.section
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0 sm:overflow-hidden`}
+        className={`${styles.padding} ${theme === "dark"? bg :""}  max-w-7xl mx-auto relative z-0 sm:overflow-hidden`}
       >
         <span className='hash-span' id={idName}>
           &nbsp;

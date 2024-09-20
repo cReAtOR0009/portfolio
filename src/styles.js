e5ca6ae478ae7a4b2e747d796fff256b288aecd4
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-09-20 04:14:16 +0100

    added text styles

--- src/styles.js
+++ src/styles.js
@@ -5,6 +5,9 @@ const styles = {
 
   heroHeadText:
     "font-black text-[25px] lg:text-[80px] sm:text-[60px] xs:text-[50px]  lg:leading-[98px] mt-2",
+    
+  heroHeadText2:
+  "text-[25px] lg:text-[35px] sm:text-[30px] xs:text-[25px]  lg:leading-[40px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[17px] lg:leading-[40px]",
 
@@ -12,6 +15,8 @@ const styles = {
     "font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[25px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    paragraph: "text-sm md:text-base lg:text-lg xl:text-lg dark:text-dark-text",
+    h3: "text-xl md:text-2xl lg:text-2xl xl:text-2xl font-semibold leading-none sm:leading-normal dark:text-dark-text",
 };
 
 export { styles };
